e67923e565442317a1fdbceddb5c5ba337d48331
TenseiCho <zellasphere@gmail.com>
2024-10-12 12:05:43 -0700

    added music/fade out

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,21 +5,29 @@ export class GameScene extends Scene
     constructor ()
     {
         super('GameScene');
+        this.music = null;
     }
 
     preload ()
     {
-        // Load the Cirno image
-        this.load.image('cirno', 'assets/cirno.png');
+        // Load the Flandre image
+        this.load.image('flandre', 'assets/flandre.png');
+        
+        // Load the Flandre theme
+        this.load.audio('flandreTheme', 'assets/Flandre_Theme.wav');
     }
 
     create ()
     {
-        // Add Cirno as the background
-        const cirno = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'cirno');
+        // Add Flandre as the background
+        const flandre = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'flandre');
         
         // Scale the image to fit the screen
-        cirno.setScale(Math.max(this.cameras.main.width / cirno.width, this.cameras.main.height / cirno.height));
+        flandre.setScale(Math.max(this.cameras.main.width / flandre.width, this.cameras.main.height / flandre.height));
+
+        // Play the Flandre theme
+        this.music = this.sound.add('flandreTheme', { loop: true });
+        this.music.play();
 
         // This is a placeholder for your game logic
         const text = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Game Scene', 
@@ -27,7 +35,19 @@ export class GameScene extends Scene
         text.setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
-            this.scene.start('MainMenu');
+            this.fadeOutMusic();
+        });
+    }
+
+    fadeOutMusic() {
+        this.tweens.add({
+            targets: this.music,
+            volume: 0,
+            duration: 1000, // 1 second fade out
+            onComplete: () => {
+                this.music.stop();
+                this.scene.start('MainMenu');
+            }
         });
     }
 }
