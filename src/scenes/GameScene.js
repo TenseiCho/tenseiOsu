29cd195a457e23931d97937e57b007bdf0c9b518
TenseiCho <zellasphere@gmail.com>
2024-10-12 11:44:39 -0700

    the start of something special

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,8 +7,20 @@ export class GameScene extends Scene
         super('GameScene');
     }
 
+    preload ()
+    {
+        // Load the Cirno image
+        this.load.image('cirno', 'assets/cirno.png');
+    }
+
     create ()
     {
+        // Add Cirno as the background
+        const cirno = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'cirno');
+        
+        // Scale the image to fit the screen
+        cirno.setScale(Math.max(this.cameras.main.width / cirno.width, this.cameras.main.height / cirno.height));
+
         // This is a placeholder for your game logic
         const text = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Game Scene', 
             { fontSize: '32px', fill: '#fff' });
